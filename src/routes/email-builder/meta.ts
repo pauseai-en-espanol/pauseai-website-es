71115dd2065efcd0c03ce15f02c05c51d2a355f6
Patricio <patriciovercesi@hotmail.com>
2026-02-19 20:00:08 -0300

    "plantilla" en vez de "constructor"

--- src/routes/email-builder/meta.ts
+++ src/routes/email-builder/meta.ts
@@ -1,10 +1,9 @@
 import type { Post } from '$lib/types'
 
 export const meta: Post = {
-	title: 'Constructor de correos electrónicos',
+	title: 'Plantilla de correos electrónicos',
 	slug: 'email-builder',
-	description:
-		'Crea un correo electrónico para enviar a tus representantes políticos y convencerles de que paren la IA.',
+	description: 'Crea un email para enviar a tus representantes políticos.',
 	date: '2023-12-08',
 	categories: []
 }
